93e207d9a0937fc75f33af3bf2e0e784a383c57c
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2021-11-18 16:49:27 -0300

    A extensão do arquivo resultava em problemas de compilação acusando erros de importação de módulos

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+
+//const { pathsToModuleNameMapper } = require('ts-jest')
+//const { compilerOptions } = require('./tsconfig')
+import {pathsToModuleNameMapper} from 'ts-jest/utils';
+import {compilerOptions} from './tsconfig.json'
+module.exports = {
+  bail: true,
+  clearMocks: true,
+  coverageProvider: "v8",
+  preset: "ts-jest",
+  testMatch: [
+    "**/*.spec.ts"
+  ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/src" }),
+
+};
