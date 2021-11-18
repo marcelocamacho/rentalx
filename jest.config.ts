0dcfcabf5d883ce6d72c2760b73d260d26b569e1
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2021-11-18 20:21:44 -0300

    continuação do merge

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@
 //const { compilerOptions } = require('./tsconfig')
 import {pathsToModuleNameMapper} from 'ts-jest/utils';
 import {compilerOptions} from './tsconfig.json'
->>>>>>> 1da3800d7cb65ad4848b8938918bc12f45fa2697:jest.config.ts
 module.exports = {
   bail: true,
   clearMocks: true,
