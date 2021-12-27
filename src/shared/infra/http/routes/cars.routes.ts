f60b8186a2047dcd7150b129de3d90444d06af06
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2021-12-27 20:52:26 -0300

    Controller com erro de token.

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,22 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
+
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
-carsRoutes.post("/", createCarController.handle);
+carsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  // ensureAdmin,
+  createCarController.handle
+);
+
+carsRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carsRoutes };
