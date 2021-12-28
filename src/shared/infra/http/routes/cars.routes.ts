3a18279a766dc2af7678c38d61125f8f8833f9e1
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2021-12-27 21:44:29 -0300

    Alteração do import para o formado @shared...

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
-
-import { ensureAdmin } from "../middleware/ensureAdmin";
-import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
+import { ensureAdmin } from "@shared/infra/http/middleware/ensureAdmin";
+import { ensureAuthenticated } from "@shared/infra/http/middleware/ensureAuthenticated";
 
 const carsRoutes = Router();
 
