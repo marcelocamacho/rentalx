d3e084fde50756245b261d611de252754528b11e
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2022-04-21 21:19:08 -0300

    Alteração do nome da coluna car_id que é chave estrangeira

--- src/shared/infra/typeorm/migrations/1640802324566-CreateCarImages.ts
+++ src/shared/infra/typeorm/migrations/1640802324566-CreateCarImages.ts
@@ -7,7 +7,7 @@ export class CreateCarImages1640802324566 implements MigrationInterface {
         name: "cars_image",
         columns: [
           { name: "id", type: "uuid", isPrimary: true },
-          { name: "cars_id", type: "uuid" },
+          { name: "car_id", type: "uuid" },
           { name: "image_name", type: "varchar" },
           { name: "created_at", type: "timestamp", default: "now()" },
         ],
@@ -16,7 +16,7 @@ export class CreateCarImages1640802324566 implements MigrationInterface {
             name: "FKCarImage",
             referencedTableName: "cars",
             referencedColumnNames: ["id"],
-            columnNames: ["id"],
+            columnNames: ["car_id"],
             onDelete: "SET NULL",
             onUpdate: "SET NULL",
           },
