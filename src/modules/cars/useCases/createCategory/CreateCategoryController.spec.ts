c319ec1acb4270eb4a1f7e9fcd15e076f7f8cbde
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2022-04-22 10:06:02 -0300

    Erro 500

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,10 @@
+import request from "supertest";
+
+import { app } from "@shared/infra/http/app";
+
+describe("Create Category Controller", () => {
+
+    it("Test", async () => {
+        await request(app).get("/cars/available").expect(200);
+    });
+});
