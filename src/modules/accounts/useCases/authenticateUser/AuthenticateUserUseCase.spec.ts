4dc8bbbbcfadaceb930213bd746c0592c84bf215
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2022-07-03 10:38:01 -0300

    Ajustes da configuração do ESLint + Prettier

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,63 +1,62 @@
-import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
-import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
-import { CreateUserUseCase } from "@modules/accounts/useCases/createUser/CreateUserUseCase";
-import { AppError } from "@shared/errors/AppError";
+import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
+import { UsersRepositoryInMemory } from '@modules/accounts/repositories/in-memory/UsersRepositoryInMemory';
+import { CreateUserUseCase } from '@modules/accounts/useCases/createUser/CreateUserUseCase';
+import { AppError } from '@shared/errors/AppError';
 
-import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
 
 let authenticateUserUseCase: AuthenticateUserUseCase;
 let userRepositoryInMemory: UsersRepositoryInMemory;
 let createUserUseCase: CreateUserUseCase;
 
-describe("Authenticate User", () => {
-  beforeEach(() => {
-    userRepositoryInMemory = new UsersRepositoryInMemory();
-    authenticateUserUseCase = new AuthenticateUserUseCase(
-      userRepositoryInMemory
-    );
-    createUserUseCase = new CreateUserUseCase(userRepositoryInMemory);
-  });
-  it("Shold be able authenticate an user", async () => {
-    const user: ICreateUserDTO = {
-      driver_license: "000123",
-      email: "user@test.com",
-      password: "1234",
-      name: "User Test",
-    };
-    await createUserUseCase.execute(user);
-
-    const result = await authenticateUserUseCase.execute({
-      email: user.email,
-      password: user.password,
+describe('Authenticate User', () => {
+    beforeEach(() => {
+        userRepositoryInMemory = new UsersRepositoryInMemory();
+        authenticateUserUseCase = new AuthenticateUserUseCase(
+            userRepositoryInMemory
+        );
+        createUserUseCase = new CreateUserUseCase(userRepositoryInMemory);
     });
 
-    expect(result).toHaveProperty("token");
-  });
+    it('Shold be able authenticate an user', async () => {
+        const user: ICreateUserDTO = {
+            driver_license: '000123',
+            email: 'user@test.com',
+            password: '1234',
+            name: 'User Test',
+        };
 
-  it("Should not be able to authenticate an nonexistente user", () => {
-    expect(async () => {
-      await authenticateUserUseCase.execute({
-        email: "false@email.com",
-        password: "1234",
-      });
-    }).rejects.toBeInstanceOf(AppError);
-  });
+        await createUserUseCase.execute(user);
+        const result = await authenticateUserUseCase.execute({
+            email: user.email,
+            password: user.password,
+        });
 
-  it("Should not be able to authenticate with incorrect password", () => {
-    expect(async () => {
-      const user: ICreateUserDTO = {
-        driver_license: "00999",
-        email: "user@user.com",
-        password: "1234",
-        name: "User Test Error",
-      };
+        expect(result).toHaveProperty('token');
+    });
 
-      await createUserUseCase.execute(user);
+    it('Should not be able to authenticate an nonexistente user', () => {
+        expect(async () => {
+            await authenticateUserUseCase.execute({
+                email: 'false@email.com',
+                password: '1234',
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
 
-      await authenticateUserUseCase.execute({
-        email: user.email,
-        password: "IncorrectPassword",
-      });
-    }).rejects.toBeInstanceOf(AppError);
-  });
+    it('Should not be able to authenticate with incorrect password', () => {
+        expect(async () => {
+            const user: ICreateUserDTO = {
+                driver_license: '00999',
+                email: 'user@user.com',
+                password: '1234',
+                name: 'User Test Error',
+            };
+            await createUserUseCase.execute(user);
+            await authenticateUserUseCase.execute({
+                email: user.email,
+                password: 'IncorrectPassword',
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
 });
