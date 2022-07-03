6c54ac106f6beaaadff4f0f2c888291afdffa79a
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2022-07-03 19:35:59 -0300

    Criação da entidade que irá armazenar os tokens

---
+++ src/modules/accounts/infra/typeorm/entities/UserTokens.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { User } from "./User";
+
+@Entity("users_tokens")
+class UserTokens {
+  @PrimaryColumn()
+  id: string;
+  @Column()
+  refresh_token: string;
+  @Column()
+  user_id: string;
+  @Column()
+  expires_date: Date;
+  @CreateDateColumn()
+  created_at: Date;
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id", referencedColumnName: "id" })
+  user: User;
+}
+export { UserTokens };
