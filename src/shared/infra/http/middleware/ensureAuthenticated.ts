c1ce309367fc488a2a13d1438be20e56d81a8188
Marcelo Santana Camacho <marcelocamacho.ufpa@gmail.com>
2022-07-03 23:20:56 -0300

    Feature: Criação do UseCase e Controller do Refresh Token

--- src/shared/infra/http/middleware/ensureAuthenticated.ts
+++ src/shared/infra/http/middleware/ensureAuthenticated.ts
@@ -3,6 +3,8 @@ import { verify } from "jsonwebtoken";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { AppError } from "@shared/errors/AppError";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
+import auth from "@config/auth";
 
 interface IPayload {
   sub: string;
@@ -14,6 +16,8 @@ export async function ensureAuthenticated(
   next: NextFunction
 ) {
   const authHeader = request.headers.authorization;
+  const userTokensRepository = new UsersTokensRepository();
+
   if (!authHeader) {
     throw new AppError("Token missing", 401);
   }
@@ -22,12 +26,15 @@ export async function ensureAuthenticated(
   try {
     const { sub: user_id } = verify(
       token,
-      "cfe275a5908b5650488e0b0342c2d6cc"
+      auth.secret_refresh_token
     ) as IPayload;
 
-    const userRepository = new UsersRepository();
+    // const userRepository = new UsersRepository();
 
-    const user = userRepository.findById(user_id);
+    const user = userTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
 
     if (!user) {
       throw new AppError("User does not exists!", 401);
